5218cd84c983960760e7dcd2b878750f30a24955
Maeve Griffin <griffinmaeve@gmail.com>
2019-08-21 10:44:14 -0700

    api refactor

--- server.js
+++ server.js
@@ -10,25 +10,24 @@ const app = express();
 // - get the port on which to run the server
 const PORT = process.env.PORT;
 
-const { getLocation } = require('./lib/geocode-api');
-console.log(getLocation);
+const mapsAPI = require('./lib/geocode-api');
+
 const weatherData = require('./data/darksky.json');
 // - enable CORS
 app.use(cors());
 
 app.get('/location', (request, response) => {
-    try {
-        const location = request.query.location;
-        const result = getLatLng(location);
-        response.status(200).json(result);
+    const search = request.query.search;
+    mapsAPI.getLocation(search)
+        .then(location => {
+            response.json(location);
+        })
 
-
-    }
-
-    catch(err) {
-        // TODO: make an object and send via .json...
-        response.status(500).send('Sorry something went wrong, please try again');
-    }
+        .catch(err => {
+            response.status(500).json({
+                error: err.message || err
+            });
+        });
 
 });
 
@@ -57,24 +56,6 @@ function getForecast() {
     return eightDayForecast;
 }
 
-function getLatLng() {
-    // ignore location for now, return hard-coded file
-    // api call will go here
-
-    return toLocation(geoData);
-}
-
-function toLocation() {
-    const firstResult = geoData.results[0];
-    const geometry = firstResult.geometry;
-    
-    return {
-        search_query: 'Alchemy',
-        formatted_query: firstResult.formatted_address,
-        latitude: geometry.location.lat,
-        longitude: geometry.location.lng
-    };
-}
 
 app.listen(PORT, () => {
     console.log('server running on PORT', PORT);
